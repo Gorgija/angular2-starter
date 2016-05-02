896a64e84e4e3341b0193e4c08d5dce9fbac82b6
Georgy Georgievski <georgievski.two@gmail.com>
2016-05-02 15:46:53 +0200

    Angular 2 start

---
+++ components/app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector:'app-body',
+    template:'Test Start'
+})
+export class AppComponent {}
---
+++ components/main.ts
@@ -0,0 +1,10 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app/app.component';
+
+
+bootstrap(AppComponent)    
+
+
+
+
+
